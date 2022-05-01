2c0eb68b292586de82868d292cb42a757270c415
0xCAF2 <pub@0xcaf2.dev>
2022-05-01 20:09:19 +0900

    define tooltips

--- src/en.js
+++ src/en.js
@@ -2,6 +2,7 @@
 import * as Blockly from 'blockly/core'
 
 Blockly.Msg.CALCIUM_ADD_TITLE = 'add to %1 by %2'
+Blockly.Msg.CALCIUM_ADD_TOOLTIP = 'Adds a value to a variable.'
 
 Blockly.Msg.CALCIUM_ARITHMETIC_ADD = 'add'
 Blockly.Msg.CALCIUM_ARITHMETIC_SUBTRACT = 'subtract'
@@ -12,8 +13,10 @@ Blockly.Msg.CALCIUM_ARITHMETIC_EXPONENTIATE = 'exponentiate'
 Blockly.Msg.CALCIUM_ARITHMETIC_REMAIN = 'remain'
 
 Blockly.Msg.CALCIUM_ASSIGN_TITLE = 'assign to %1 from %2'
+Blockly.Msg.CALCIUM_ASSIGN_TOOLTIP = 'Assigns data to a variable.'
 
 Blockly.Msg.CALCIUM_ATTRIBUTE_TITLE = "%1 's attribute %2"
+Blockly.Msg.CALCIUM_ATTRIBUTE_TOOLTIP = 'Accesses the attribute of an object.'
 
 Blockly.Msg.CALCIUM_BOOLEAN_TRUE = 'Yes'
 Blockly.Msg.CALCIUM_BOOLEAN_FALSE = 'No'
@@ -22,44 +25,70 @@ Blockly.Msg.CALCIUM_BREAK = 'break this loop'
 Blockly.Msg.CALCIUM_CONTINUE = 'continue next loop'
 
 Blockly.Msg.CALCIUM_CALL_TITLE = 'call %1'
+Blockly.Msg.CALCIUM_CALL_TOOLTIP = 'Calls a function.'
 Blockly.Msg.CALCIUM_CALL_ARG_TITLE = 'argument'
+Blockly.Msg.CALCIUM_CALL_ARG_TOOLTIP = 'An argument for the function call.'
 Blockly.Msg.CALCIUM_CALL_RETURN_TITLE = '%1 = call %2'
+Blockly.Msg.CALCIUM_CALL_RETURN_TOOLTIP =
+  'Assigns the result of the function call to a variable.'
 
 Blockly.Msg.CALCIUM_CLASS_TITLE = '%1 class'
+Blockly.Msg.CALCIUM_CLASS_TOOLTIP =
+  'Specifies the name of a class to be instantiated.'
 
 Blockly.Msg.CALCIUM_CLASS_DEF_TITLE = 'define %1 class %2 inherit %3 %4'
+Blockly.Msg.CALCIUM_CLASS_DEF_TOOLTIP = 'Defines a class.'
 
 Blockly.Msg.CALCIUM_DEF_TITLE = 'define %1 function %2 %3 %4'
+Blockly.Msg.CALCIUM_DEF_TOOLTIP = 'Defines a function.'
 Blockly.Msg.CALCIUM_DEF_METHOD_TITLE = 'define %1 method %2 %3 %4'
+Blockly.Msg.CALCIUM_DEF_METHOD_TOOLTIP = 'Defines a method.'
 Blockly.Msg.CALCIUM_DEF_PARAM_TITLE = 'parameter %1'
+Blockly.Msg.CALCIUM_DEF_PARAM_TOOLTIP = 'A parameter of the function.'
 
 Blockly.Msg.CALCIUM_DICT_TITLE = 'create a dict'
+Blockly.Msg.CALCIUM_DICT_TOOLTIP = 'Creates an empty dict.'
 
 Blockly.Msg.CALCIUM_FOR_TITLE = 'repeat %1 from %2 %3 %4'
+Blockly.Msg.CALCIUM_FOR_TOOLTIP = 'Repeats by extracting data to a variable.'
 
 Blockly.Msg.CALCIUM_FOR_RANGE_TITLE =
   'repeat for %1 from %2 , to %3 , by %4 %5 %6'
+Blockly.Msg.CALCIUM_FOR_RANGE_TOOLTIP =
+  'Repeats ranges from the start to the end by the step value.'
 
 Blockly.Msg.CALCIUM_FUNCTION_TITLE = '%1 function'
+Blockly.Msg.CALCIUM_FUNCTION_TOOLTIP = 'Specifies the name of a function.'
 
 Blockly.Msg.CALCIUM_IF_TITLE = 'if %1:'
+Blockly.Msg.CALCIUM_IF_TOOLTIP =
+  'Checks the condition and decides whether to execute.'
 Blockly.Msg.CALCIUM_IF_IF_TITLE = 'if'
+Blockly.Msg.CALCIUM_IF_IF_TOOLTIP = 'The first condition.'
 Blockly.Msg.CALCIUM_IF_ELSEIF_TITLE = 'else if'
+Blockly.Msg.CALCIUM_IF_ELSEIF_TOOLTIP = 'The condition of the second or later.'
 Blockly.Msg.CALCIUM_IF_ELSE_TITLE = 'else'
+Blockly.Msg.CALCIUM_IF_ELSE_TOOLTIP = 'Excutes when any condition has been met.'
 
 Blockly.Msg.CALCIUM_IMPORT_TITLE = 'import %1'
+Blockly.Msg.CALCIUM_IMPORT_TOOLTIP = 'Uses libraries or modules.'
 
 Blockly.Msg.CALCIUM_LIST_TITLE = 'create a list'
+Blockly.Msg.CALCIUM_LIST_TOOLTIP = 'Creates a list.'
 Blockly.Msg.CALCIUM_LIST_ITEM_TITLE = 'element'
+Blockly.Msg.CALCIUM_LIST_ITEM_TOOLTIP = 'The element of a list.'
 
 Blockly.Msg.CALCIUM_LOGICAL_AND = 'and'
 Blockly.Msg.CALCIUM_LOGICAL_OR = 'or'
 
 Blockly.Msg.CALCIUM_METHOD_TITLE = "%1 's %2 method"
+Blockly.Msg.CALCIUM_METHOD_TOOLTIP = 'Specifies the name of a method.'
 
 Blockly.Msg.CALCIUM_NOT_TITLE = 'not %1'
+Blockly.Msg.CALCIUM_NOT_TOOLTIP = 'Negates a logical value.'
 
 Blockly.Msg.CALCIUM_PRINT_TITLE = 'print %1'
+Blockly.Msg.CALCIUM_PRINT_TOOLTIP = 'Outputs values as strings.'
 
 Blockly.Msg.CALCIUM_RELATIONAL_EQUAL = 'equal'
 Blockly.Msg.CALCIUM_RELATIONAL_NOT_EQUAL = 'not equal'
@@ -71,12 +100,16 @@ Blockly.Msg.CALCIUM_RELATIONAL_IN = 'contained in'
 Blockly.Msg.CALCIUM_RELATIONAL_NOT_IN = 'not contained in'
 
 Blockly.Msg.CALCIUM_RETURN_TITLE = 'return %1 from this function'
+Blockly.Msg.CALCIUM_RETURN_TOOLTIP = 'Returns a value and ends a function call.'
 
 Blockly.Msg.CALCIUM_SLICE_TITLE = 'slice %1 : %2'
+Blockly.Msg.CALCIUM_SLICE_TOOLTIP = 'Uses slicing of a list.'
 
 Blockly.Msg.CALCIUM_STR_TITLE = 'string "%1"'
+Blockly.Msg.CALCIUM_STR_TOOLTIP = 'A string value.'
 
 Blockly.Msg.CALCIUM_SUBSCRIPT_TITLE = "%1 's element [ %2 ]"
+Blockly.Msg.CALCIUM_SUBSCRIPT_TOOLTIP = 'Accesses an element of data.'
 
 Blockly.Msg.CALCIUM_UI_APP_TITLE = 'Calcium+'
 Blockly.Msg.CALCIUM_UI_OPEN = 'Open'
@@ -84,5 +117,7 @@ Blockly.Msg.CALCIUM_UI_RUN = 'Execute Program'
 Blockly.Msg.CALCIUM_UI_SAVE = 'Save'
 
 Blockly.Msg.CALCIUM_VARIABLE_TITLE = 'variable %1'
+Blockly.Msg.CALCIUM_VARIABLE_TOOLTIP = 'Specifies the name of a variable.'
 
 Blockly.Msg.CALCIUM_WHILE_TITLE = 'while %1 %2 repeat %3'
+Blockly.Msg.CALCIUM_WHILE_TOOLTIP = 'Repeats while the condition meets.'
--- src/ja.js
+++ src/ja.js
@@ -2,6 +2,7 @@
 import * as Blockly from 'blockly/core'
 
 Blockly.Msg.CALCIUM_ADD_TITLE = '%1 に %2 をたす'
+Blockly.Msg.CALCIUM_ADD_TOOLTIP = '変数にデータを足して、更新します'
 
 Blockly.Msg.CALCIUM_ARITHMETIC_ADD = 'たす'
 Blockly.Msg.CALCIUM_ARITHMETIC_SUBTRACT = 'ひく'
@@ -12,8 +13,10 @@ Blockly.Msg.CALCIUM_ARITHMETIC_EXPONENTIATE = 'べき乗'
 Blockly.Msg.CALCIUM_ARITHMETIC_REMAIN = 'わったあまり'
 
 Blockly.Msg.CALCIUM_ASSIGN_TITLE = '%1 を %2 にする'
+Blockly.Msg.CALCIUM_ASSIGN_TOOLTIP = '変数等のデータを書きかえます'
 
 Blockly.Msg.CALCIUM_ATTRIBUTE_TITLE = '%1 の属性 %2'
+Blockly.Msg.CALCIUM_ATTRIBUTE_TOOLTIP = 'オブジェクトの属性を使います'
 
 Blockly.Msg.CALCIUM_BOOLEAN_TRUE = 'はい'
 Blockly.Msg.CALCIUM_BOOLEAN_FALSE = 'いいえ'
@@ -23,32 +26,51 @@ Blockly.Msg.CALCIUM_CONTINUE = '次のループへ進む'
 
 Blockly.Msg.CALCIUM_CALL_TITLE = '%1 を呼び出す'
 Blockly.Msg.CALCIUM_CALL_ARG_TITLE = '引数'
+Blockly.Msg.CALCIUM_CALL_ARG_TOOLTIP = '関数やメソッドに渡す引数です'
 Blockly.Msg.CALCIUM_CALL_RETURN_TITLE = '%1 = %2 を呼び出す'
+Blockly.Msg.CALCIUM_CALL_RETURN_TOOLTIP =
+  '関数を呼び出して、結果を変数等に入れます'
 
 Blockly.Msg.CALCIUM_CLASS_TITLE = '%1 クラス'
+Blockly.Msg.CALCIUM_CLASS_TOOLTIP =
+  'クラスからインスタンスを作る時などに使います'
 
 Blockly.Msg.CALCIUM_CLASS_DEF_TITLE =
   'クラス %1 を定義 %2 スーパークラスは %3 %4'
+Blockly.Msg.CALCIUM_CLASS_DEF_TOOLTIP = 'クラスを定義します'
 
 Blockly.Msg.CALCIUM_DEF_TITLE = '関数 %1 を定義 %2 %3 %4'
+Blockly.Msg.CALCIUM_DEF_TOOLTIP = '関数を定義します'
 Blockly.Msg.CALCIUM_DEF_METHOD_TITLE = 'メソッド %1 を定義 %2 %3 %4'
+Blockly.Msg.CALCIUM_DEF_METHOD_TOOLTIP = 'クラスの中のメソッドを定義します'
 Blockly.Msg.CALCIUM_DEF_PARAM_TITLE = '引数 %1'
+Blockly.Msg.CALCIUM_DEF_PARAM_TOOLTIP = '関数やメソッドが受け取る引数です'
 
 Blockly.Msg.CALCIUM_DICT_TITLE = '辞書を作る'
+Blockly.Msg.CALCIUM_DICT_TOOLTIP = '空の辞書を作ります'
 
 Blockly.Msg.CALCIUM_FOR_TITLE = '%1 に %2 から取り出して %3 くりかえす %4'
+Blockly.Msg.CALCIUM_FOR_TOOLTIP = '変数にデータを取り出しながら、くり返します'
 
 Blockly.Msg.CALCIUM_FOR_RANGE_TITLE =
   '%1 を %2 から %3 まで %4 ずつ %5 くりかえす %6'
+Blockly.Msg.CALCIUM_FOR_RANGE_TOOLTIP = '指定した回数、くり返します'
 
 Blockly.Msg.CALCIUM_FUNCTION_TITLE = '%1 関数'
+Blockly.Msg.CALCIUM_FUNCTION_TOOLTIP = '関数を呼び出す時に使います'
 
 Blockly.Msg.CALCIUM_IF_TITLE = 'もし %1:'
+Blockly.Msg.CALCIUM_IF_TOOLTIP = '条件によって、場合分けします'
 Blockly.Msg.CALCIUM_IF_IF_TITLE = 'もし'
+Blockly.Msg.CALCIUM_IF_IF_TOOLTIP = '1つ目の条件です'
 Blockly.Msg.CALCIUM_IF_ELSEIF_TITLE = 'そうでなくて、もし'
+Blockly.Msg.CALCIUM_IF_ELSEIF_TOOLTIP = '2つ目以降の条件です'
 Blockly.Msg.CALCIUM_IF_ELSE_TITLE = 'それ以外'
+Blockly.Msg.CALCIUM_IF_ELSE_TOOLTIP =
+  'どれにも当てはまらなかった時、実行されます'
 
 Blockly.Msg.CALCIUM_IMPORT_TITLE = '%1 を使う'
+Blockly.Msg.CALCIUM_IMPORT_TOOLTIP = 'ライブラリやモジュールを使います'
 
 Blockly.Msg.CALCIUM_LIST_TITLE = 'リストを作る'
 Blockly.Msg.CALCIUM_LIST_ITEM_TITLE = 'リストの要素'
@@ -57,10 +79,13 @@ Blockly.Msg.CALCIUM_LOGICAL_AND = 'かつ'
 Blockly.Msg.CALCIUM_LOGICAL_OR = 'または'
 
 Blockly.Msg.CALCIUM_METHOD_TITLE = '%1 の %2 メソッド'
+Blockly.Msg.CALCIUM_METHOD_TOOLTIP = 'メソッドを呼び出す時に使います'
 
 Blockly.Msg.CALCIUM_NOT_TITLE = '%1 でない'
+Blockly.Msg.CALCIUM_NOT_TOOLTIP = '論理値を否定します'
 
 Blockly.Msg.CALCIUM_PRINT_TITLE = '表示する %1'
+Blockly.Msg.CALCIUM_PRINT_TOOLTIP = '文字列として出力します'
 
 Blockly.Msg.CALCIUM_RELATIONAL_EQUAL = '同じ'
 Blockly.Msg.CALCIUM_RELATIONAL_NOT_EQUAL = 'ちがう'
@@ -72,12 +97,16 @@ Blockly.Msg.CALCIUM_RELATIONAL_IN = 'ふくまれる'
 Blockly.Msg.CALCIUM_RELATIONAL_NOT_IN = 'ふくまれない'
 
 Blockly.Msg.CALCIUM_RETURN_TITLE = '%1 を返して、関数を終了'
+Blockly.Msg.CALCIUM_RETURN_TOOLTIP = '関数からデータを返して、関数を終了します'
 
 Blockly.Msg.CALCIUM_SLICE_TITLE = 'スライス %1 : %2'
+Blockly.Msg.CALCIUM_SLICE_TOOLTIP = 'リストのスライスを使います'
 
 Blockly.Msg.CALCIUM_STR_TITLE = '文字列 "%1"'
+Blockly.Msg.CALCIUM_STR_TOOLTIP = '文字列を使います'
 
 Blockly.Msg.CALCIUM_SUBSCRIPT_TITLE = '%1 の要素 [ %2 ]'
+Blockly.Msg.CALCIUM_SUBSCRIPT_TOOLTIP = 'リストや辞書などの要素を指定します'
 
 Blockly.Msg.CALCIUM_UI_APP_TITLE = 'カルシウム＋'
 Blockly.Msg.CALCIUM_UI_OPEN = '開く'
@@ -85,5 +114,7 @@ Blockly.Msg.CALCIUM_UI_RUN = 'プログラムを実行'
 Blockly.Msg.CALCIUM_UI_SAVE = '保存する'
 
 Blockly.Msg.CALCIUM_VARIABLE_TITLE = '変数 %1'
+Blockly.Msg.CALCIUM_VARIABLE_TOOLTIP = '変数の名前を指定します'
 
 Blockly.Msg.CALCIUM_WHILE_TITLE = '%1 の間 %2 くりかえす %3'
+Blockly.Msg.CALCIUM_WHILE_TOOLTIP = '条件を満たす間、くり返します'
