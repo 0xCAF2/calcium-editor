9f3f9dc1a0673421c251214bba1851bf7b09e183
0xCAF2 <pub@0xcaf2.dev>
2023-10-11 16:37:42 +0900

    migrate to setup syntax

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # calcium-editor
 
-A visual programming editor for learning Python on Web
+A visual programming editor for learning Python on a browser
+
+## Hosted web pages
+
+- [日本語版カルシウム](https://calcium-editor.web.app/)
+- [English ver.](https://calcium-editor.web.app/en/)
--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,175 @@
+<script setup lang="ts">
+import * as Blockly from 'blockly/core'
+import * as Lang from 'blockly/msg/ja'
+import './ja'
+import DarkTheme from '@blockly/theme-dark'
+import { calciumBlocks } from './blocks'
+import { calciumToolbox } from './toolbox'
+import { CalciumGenerator } from './calcium-generator'
+import { PseudoGenerator } from './pseudo-generator'
+import { onMounted, ref, watch } from 'vue'
+import './renderer'
+
+Blockly.setLocale(Lang)
+
+let workspace: Blockly.WorkspaceSvg | null = null
+let worker: Worker | null = null
+const pseudoGenerator = new PseudoGenerator('pseudo')
+const calciumGenerator = new CalciumGenerator('calcium')
+
+const code = ref('')
+const error = ref('')
+const input = ref('')
+const inputting = ref(false)
+const output = ref('')
+const overlayed = ref(false)
+const pseudoCode = ref('')
+const prompt = ref('')
+const running = ref(false)
+const waiting = ref(true)
+
+const labelForOpen = Blockly.Msg.CALCIUM_UI_OPEN
+const labelForReset = Blockly.Msg.CALCIUM_UI_RESET
+const labelForRun = Blockly.Msg.CALCIUM_UI_RUN
+const labelForSave = Blockly.Msg.CALCIUM_UI_SAVE
+
+function open() {
+  const inputFile = document.createElement('input')
+  inputFile.type = 'file'
+  inputFile.addEventListener('change', () => {
+    const file = inputFile.files?.[0]
+    if (!file) return
+    if (file.type !== 'application/json') {
+      alert('ファイルを開くことができませんでした')
+      return
+    }
+    const reader = new FileReader()
+    reader.addEventListener('load', () => {
+      const json = reader.result
+      if (typeof json === 'string') {
+        Blockly.serialization.workspaces.load(JSON.parse(json), workspace!)
+      }
+    })
+    reader.readAsText(file)
+  })
+  inputFile.click()
+}
+
+function resetRuntime() {
+  worker?.terminate()
+  worker = null
+  running.value = false
+  waiting.value = true
+  worker = new Worker('/script/worker.js')
+  worker.onmessage = (event) => {
+    const message = event.data
+    if (message.loaded) {
+      waiting.value = false
+    } else if (message.output || message.output === '') {
+      output.value += message.output
+      output.value += '\n'
+    } else if (message.error) {
+      error.value += message.error
+      error.value += '\n'
+    } else if (message.input || message.input === '') {
+      input.value = ''
+      prompt.value = message.input
+      inputting.value = true
+      overlayed.value = true
+    }
+  }
+}
+
+function resize() {
+  const divBlockly = document.querySelector('#div-blockly') as HTMLDivElement
+  divBlockly.style.height = window.innerHeight - 80 + 'px'
+  divBlockly.style.width = window.innerWidth - 8 + 'px'
+  Blockly.svgResize(workspace!)
+}
+
+function run() {
+  const _pseudoCode = pseudoGenerator.workspaceToCode(workspace!)
+  pseudoCode.value = _pseudoCode
+
+  const jsonCode = calciumGenerator.workspaceToCode(workspace!)
+  code.value = jsonCode
+  const _code = `runtime = Runtime('${jsonCode
+    .replace(/([^\\])\n/g, '$1')
+    .replace(/'/g, "\\'")}')
+result = runtime.run()
+print(end='', flush=True)
+result.value
+`
+  worker?.postMessage({ code: _code })
+}
+
+function save() {
+  const json = Blockly.serialization.workspaces.save(workspace!)
+  const a = document.createElement('a')
+  a.href =
+    'data:application/json;charset=utf-8,' +
+    encodeURIComponent(JSON.stringify(json))
+  a.download = 'code.json'
+  a.click()
+}
+
+function sendInput() {
+  inputting.value = false
+  overlayed.value = false
+  worker?.postMessage({
+    input: input.value.replace(/\n/g, '').replace(/'/g, "\\'"),
+  })
+}
+
+onMounted(() => {
+  Blockly.common.defineBlocks(calciumBlocks)
+  workspace = Blockly.inject(document.querySelector('#div-blockly')!, {
+    move: {
+      scrollbars: true,
+      drag: true,
+      wheel: false,
+    },
+    zoom: {
+      controls: true,
+      startScale: 0.7,
+    },
+    renderer: 'calcium_renderer',
+    sounds: false,
+    theme: DarkTheme as any,
+    toolbox: {
+      kind: 'categoryToolbox',
+      contents: calciumToolbox,
+    },
+  })
+  resize()
+  window.addEventListener('resize', resize, false)
+  window.addEventListener('beforeunload', function (e) {
+    e.preventDefault()
+    e.returnValue = 'c'
+  })
+  resetRuntime()
+})
+
+watch(running, (newValue) => {
+  if (newValue) {
+    output.value = ''
+    run()
+  } else {
+    inputting.value = false
+  }
+  error.value = ''
+})
+
+watch(
+  overlayed, (newValue) => {
+    if (inputting.value) {
+      overlayed.value = true
+    } else {
+      overlayed.value = newValue
+    }
+  })
+</script>
+
 <template>
   <v-app>
     <v-app-bar app :color="running ? 'orange' : 'blue'" style="z-index: 3000">
@@ -30,7 +202,7 @@
         </v-menu>
       </v-app-bar-title>
       <template v-slot:append>
-        <v-btn icon href="https://calcium.0xcaf2.dev" target="_blank">
+        <v-btn icon href="https://calcium.0xcaf2.dev/" target="_blank">
           <span><b>？</b></span>
         </v-btn>
       </template>
@@ -65,179 +237,6 @@
   </v-app>
 </template>
 
-<script>
-import * as Blockly from 'blockly/core'
-import * as Lang from 'blockly/msg/ja'
-import './ja'
-import DarkTheme from '@blockly/theme-dark'
-// import definition from './definition_ja'
-import { calciumBlocks } from './blocks'
-import { calciumToolbox } from './toolbox'
-// import generator from './generator'
-import { CalciumGenerator } from './calcium-generator'
-import { PseudoGenerator } from './pseudo-generator'
-import { defineComponent } from 'vue'
-import './renderer'
-
-Blockly.setLocale(Lang)
-
-let workspace = null
-const pseudoGenerator = new PseudoGenerator('pseudo')
-const calciumGenerator = new CalciumGenerator('calcium')
-
-export default defineComponent({
-  name: 'App',
-  data: () => ({
-    debuggerEnabled: false,
-    code: '',
-    error: '',
-    input: '',
-    inputting: false,
-    output: '',
-    overlayed: false,
-    pseudoCode: '',
-    prompt: '',
-    running: false,
-    waiting: true,
-  }),
-  computed: {
-    labelForOpen: () => Blockly.Msg.CALCIUM_UI_OPEN,
-    labelForReset: () => Blockly.Msg.CALCIUM_UI_RESET,
-    labelForRun: () => Blockly.Msg.CALCIUM_UI_RUN,
-    labelForSave: () => Blockly.Msg.CALCIUM_UI_SAVE,
-  },
-  methods: {
-    open() {
-      const inputFile = document.createElement('input')
-      inputFile.type = 'file'
-      inputFile.addEventListener('change', () => {
-        const file = inputFile.files?.[0]
-        if (!file) return
-        if (file.type !== 'application/json') {
-          alert('ファイルを開くことができませんでした')
-          return
-        }
-        const reader = new FileReader()
-        reader.addEventListener('load', () => {
-          const json = reader.result
-          if (typeof json === 'string') {
-            Blockly.serialization.workspaces.load(JSON.parse(json), workspace)
-          }
-        })
-        reader.readAsText(file)
-      })
-      inputFile.click()
-    },
-    resetRuntime() {
-      this.worker?.terminate()
-      this.worker = null
-      this.running = false
-      this.waiting = true
-      this.worker = new Worker('/script/worker.js')
-      this.worker.onmessage = (event) => {
-        const message = event.data
-        if (message.loaded) {
-          this.waiting = false
-        } else if (message.output || message.output === '') {
-          this.output += message.output
-          this.output += '\n'
-        } else if (message.error) {
-          this.error += message.error
-          this.error += '\n'
-        } else if (message.input || message.input === '') {
-          this.input = ''
-          this.prompt = message.input
-          this.inputting = true
-          this.overlayed = true
-        }
-      }
-    },
-    resize() {
-      const divBlockly = document.querySelector('#div-blockly')
-      divBlockly.style.height = window.innerHeight - 80 + 'px'
-      divBlockly.style.width = window.innerWidth - 8 + 'px'
-      Blockly.svgResize(workspace)
-    },
-    run() {
-      const pseudoCode = pseudoGenerator.workspaceToCode(workspace)
-      this.pseudoCode = pseudoCode
-
-      const jsonCode = calciumGenerator.workspaceToCode(workspace)
-      this.code = jsonCode
-      const code = `runtime = Runtime('${jsonCode
-        .replace(/([^\\])\n/g, '$1')
-        .replace(/'/g, "\\'")}')
-result = runtime.run()
-print(end='', flush=True)
-result.value
-`
-      this.worker.postMessage({ code })
-    },
-    save() {
-      const json = Blockly.serialization.workspaces.save(workspace)
-      const a = document.createElement('a')
-      a.href =
-        'data:application/json;charset=utf-8,' +
-        encodeURIComponent(JSON.stringify(json))
-      a.download = 'code.json'
-      a.click()
-    },
-    sendInput() {
-      this.inputting = false
-      this.overlayed = false
-      this.worker.postMessage({
-        input: this.input.replace(/\n/g, '').replace(/'/g, "\\'"),
-      })
-    },
-  },
-  mounted() {
-    Blockly.common.defineBlocks(calciumBlocks)
-    workspace = Blockly.inject(document.querySelector('#div-blockly'), {
-      move: {
-        scrollbars: true,
-        drag: true,
-        wheel: false,
-      },
-      zoom: {
-        controls: true,
-        startScale: 0.7,
-      },
-      renderer: 'calcium_renderer',
-      sounds: false,
-      theme: DarkTheme,
-      toolbox: {
-        kind: 'categoryToolbox',
-        contents: calciumToolbox,
-      },
-    })
-    this.resize()
-    window.addEventListener('resize', this.resize, false)
-    window.addEventListener('beforeunload', function (e) {
-      e.preventDefault()
-      e.returnValue = 'c'
-    })
-    this.resetRuntime()
-  },
-  watch: {
-    running(newValue) {
-      if (newValue) {
-        this.output = ''
-        this.run()
-      } else {
-        this.inputting = false
-      }
-      this.error = ''
-    },
-    overlayed(newValue) {
-      if (this.inputting) {
-        this.overlayed = true
-      } else {
-        this.overlayed = newValue
-      }
-    },
-  },
-})
-</script>
 <style scoped>
 #output {
   position: absolute;
