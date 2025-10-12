a13948148e1020ddbee0f0f62a97d91bcea9144e
Roki Turner <roki@0xcaf2.dev>
2025-10-12 10:56:51 +0900

    feat: PoC

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,19 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:webview_flutter_web/webview_flutter_web.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends HookWidget {
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    final controller = useMemoized(() => WebViewController());
+    useEffect(() {
+      WebViewPlatform.instance = WebWebViewPlatform();
+      controller.loadRequest(Uri.parse('http://localhost:5173'));
+      return null;
+    }, const []);
+
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+        body: Center(child: WebViewWidget(controller: controller)),
       ),
     );
   }
