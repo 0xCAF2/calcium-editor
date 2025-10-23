cd8a0e3c685c5006767426da877dcb018e1d969d
Roki Turner <roki@0xcaf2.dev>
2025-10-23 14:47:11 +0900

    update: revert to vanilla TS

--- src/ui/menu.ts
+++ src/ui/menu.ts
@@ -1,26 +1,6 @@
 import type { L10N } from "../l10n"
-import { html, LitElement } from "lit"
-import { customElement } from "lit/decorators.js"
+import "./run-button"
 
 const menu = document.querySelector("#menu") as HTMLElement
 
-export function createMenu(l10n: L10N) {
-  const menuBar = new MenuBar(l10n)
-  menu.appendChild(menuBar)
-}
-
-@customElement("menu-bar")
-class MenuBar extends LitElement {
-  constructor(private l10n: L10N) {
-    super()
-  }
-
-  render() {
-    return html`
-      <div id="menu">
-        <button id="run-button">${this.l10n.run}</button>
-        <button id="stop-button">${this.l10n.stop}</button>
-      </div>
-    `
-  }
-}
+export function createMenu(l10n: L10N) {}
