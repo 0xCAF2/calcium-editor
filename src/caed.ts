2e9bfb241a145828602ed8b4df798a66c665ebc0
Roki Turner <roki@0xcaf2.dev>
2026-06-22 14:08:28 +0900

    feat: add new class

--- src/caed.ts
+++ src/caed.ts
@@ -1,1 +1,19 @@
-export class Caed {}
+export abstract class Caed {
+  public options = new CaedOptions()
+
+  build() {
+    throw new Error("Method not implemented.")
+  }
+
+  set height(value: string | number) {
+    if (typeof value === "number") {
+      this.options.height = `${value}px`
+    } else {
+      this.options.height = value
+    }
+  }
+}
+
+export class CaedOptions {
+  public height?: string
+}
